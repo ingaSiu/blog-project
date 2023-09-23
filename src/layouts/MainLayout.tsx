4ff70a0358d3e74ef7019771b56b5485d8c7db8c
ingaSiu <ingasiud@gmail.com>
2023-09-23 14:17:41 +0300

    add links to main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,14 +1,39 @@
-import Navbar from '../components/Navbar/Navbar';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 
+import { HOME_PATH } from '../routes/consts';
+import Navbar from '../components/Navbar/Navbar';
+
+export type LinksProps = {
+  path: string;
+  title: string;
+};
 type MainLayoutProps = {
   children: ReactNode;
 };
 
 const MainLayout = ({ children }: MainLayoutProps) => {
+  const links: LinksProps[] = [
+    {
+      path: HOME_PATH,
+      title: 'RECIPES',
+    },
+    {
+      path: HOME_PATH,
+      title: 'test1',
+    },
+    {
+      path: HOME_PATH,
+      title: 'test 2',
+    },
+  ];
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [children]);
+
   return (
     <>
-      <Navbar />
+      <Navbar links={links} homeLink={HOME_PATH} />
       <div>{children}</div>;
     </>
   );
